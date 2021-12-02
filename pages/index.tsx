2da1816c543ea388fa5b797c2179dc3a10ef46a0
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-12-02 12:13:51 +0530

    Chakra UI setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,16 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { Heading } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  return <div>NOTEVID</div>;
+  return (
+    <div>
+      <Heading as="h1" size="xl" isTruncated>
+        NoteVid
+      </Heading>
+    </div>
+  );
 };
 
 export default Home;
