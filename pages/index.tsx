02c4e0f90e4d7e1c84b57cc76e3d0b043cb23fb9
Sayuri <sayurikamble123@gmail.com>
2024-01-27 22:22:01 +0530

    SSR Rendering for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,46 @@
-import type { NextPage } from "next";
-import {
-  Box,
-  ColorModeScript,
-  SimpleGrid,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
 import Head from "next/head";
-import theme from "../config/chakraConfig";
 import { Navbar } from "../components/navbar";
 import VideoCard from "../components/videoCard";
-import { useVideos } from "../context/videosContext";
+import { IVideoArr } from "../types";
 
-const Home: NextPage = () => {
-  const { videos } = useVideos();
+export async function getServerSideProps() {
+  const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API;
+  const searchQuery = encodeURIComponent("coding, podcast, upsc");
+  const maxResults = 100;
+  const regionCode = "in";
+
+  const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${searchQuery}&key=${apiKey}&regionCode=${regionCode}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const videos = data.items
+      .filter((video: any) => video.id.videoId)
+      .map((video: any) => ({
+        videoId: video.id.videoId,
+        channelId: video.snippet.channelId,
+        channelTitle: video.snippet.channelTitle,
+        description: video.snippet.description,
+        publishTime: video.snippet.publishTime,
+        thumbnail: video.snippet.thumbnails.medium.url,
+        title: video.snippet.title,
+      }));
+
+    return { props: { videos } };
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return { props: { videoData: [] } };
+  }
+}
+
+export default function Page({ videos }: { videos: IVideoArr[] }) {
 
   return (
     <Box backgroundColor={useColorModeValue("gray.100", "black.100")}>
@@ -46,6 +74,4 @@ const Home: NextPage = () => {
       </SimpleGrid>
     </Box>
   );
-};
-
-export default Home;
+}
